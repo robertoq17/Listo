d7c3435f84d4e6a3587aba5ef6ff12bfdbd21012
robertoq17 <robertoq17@gmail.com>
2019-02-23 10:33:54 -0400

    added push notification with redirect

--- Listo/Listo/App.xaml.cs
+++ Listo/Listo/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Listo.Views;
+﻿using Listo.ViewModels;
+using Listo.Views;
 using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
@@ -12,6 +13,8 @@ namespace Listo
 {
     public partial class App : Application
     {
+        string pushValue;
+
         public App()
         {
             //Register Syncfusion license
@@ -20,12 +23,31 @@ namespace Listo
             InitializeComponent();
 
             MainPage = new NavigationPage(new CategoryPage());
+
+
         }
 
         protected override void OnStart()
         {
             // This should come before AppCenter.Start() is called
             // Avoid duplicate event registration:
+            ReceivedMessageWithAppCenter();
+
+
+
+            // AppCenter.start after
+            // Handle when your app starts
+            AppCenter.Start("android=ba6eb727-fecd-4e0b-8935-623b67d57036;" +
+                "uwp=d1f0d766-831d-4ded-9e34-5cca0d89bd3d;" +
+                "ios={Your iOS App secret here}",
+                typeof(Push),
+                typeof(Analytics),
+                typeof(Crashes)
+                );
+        }
+
+        private void ReceivedMessageWithAppCenter()
+        {
             if (!AppCenter.Configured)
             {
                 Push.PushNotificationReceived += (sender, e) =>
@@ -43,6 +65,18 @@ namespace Listo
                         foreach (var key in e.CustomData.Keys)
                         {
                             summary += $"\t\t{key} : {e.CustomData[key]}\n";
+
+                            //Here su caught the value
+                            if (string.IsNullOrEmpty(pushValue))
+                            {
+                                pushValue = key;
+
+                                if (!string.IsNullOrEmpty(pushValue) && pushValue.ToLower() == "sub")
+                                {
+                                    MainViewModel.GetInstance().Rating = new RatingViewModel();
+                                    Current.MainPage.Navigation.PushAsync(new RatingPage());
+                                }
+                            }
                         }
                     }
 
@@ -50,16 +84,6 @@ namespace Listo
                     System.Diagnostics.Debug.WriteLine(summary);
                 };
             }
-
-            // AppCenter.start after
-            // Handle when your app starts
-            AppCenter.Start("android=ba6eb727-fecd-4e0b-8935-623b67d57036;" +
-                "uwp=d1f0d766-831d-4ded-9e34-5cca0d89bd3d;" +
-                "ios={Your iOS App secret here}",
-                typeof(Push),
-                typeof(Analytics),
-                typeof(Crashes)
-                );
         }
 
         protected override void OnSleep()
